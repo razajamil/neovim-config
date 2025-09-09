7fdc934124603fab7152ced048d1bf84eb7f8ba0
NavaceSystem <software@navace.com.au>
2025-09-09 10:47:28 +1000

    stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,9 @@ return {
   { "mason-org/mason.nvim", version = "^1.0.0" },
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
-    inlay_hints = { enabled = false },
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
   },
 }
