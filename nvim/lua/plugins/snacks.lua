a7439a6ef0db2592c2871b4f804a805d51aade94
NavaceSystem <software@navace.com.au>
2026-03-22 11:12:00 +1100

    switched to fff.nvim for file search and grep

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,5 +2,21 @@ return {
   "folke/snacks.nvim",
   opts = {
     gh = {},
+    picker = {
+      sources = {
+        explorer = { hidden = true },
+      },
+    },
+  },
+  keys = {
+    -- Disable file search keys because we use fff.nvim for that
+    { "<leader>ff", mode = { "n" }, false },
+    { "<leader>fF", mode = { "n" }, false },
+    { "<leader><leader>", mode = { "n" }, false },
+    { "<leader>fg", mode = { "n" }, false },
+    { "<leader>fc", mode = { "n" }, false },
+
+    -- Disable grep keys because we use fff.nvim for that
+    { "<leader>/", mode = { "n" }, false },
   },
 }
